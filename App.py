a9440a1136b3f697904d12d1d5d327ba3988d79f
raghuvamsilakhinana <raghuvamsi.lakhinana@gmail.com>
2022-08-05 12:50:50 +0530

    product code done

--- App.py
+++ App.py
@@ -9,3 +9,9 @@ sum = num1 + num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
+# Multiply two numbers
+prod = num1 * num2
+
+# Display the sum
+print('The product of {0} and {1} is {2}'.format(num1, num2, prod))
+
